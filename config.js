b531b08d9b921903a7b0144bb025053731d177e2
Taher <taherraja41@gmail.com>
2026-02-13 18:55:29 -0800

    music added

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://audio.com/shamas/audio/main-agar-kahoon-om-shanti-om-320-kbps", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/ddszz8etz/video/upload/v1771037657/MEIN_AGAR_KAHOON_v9kvmx.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
